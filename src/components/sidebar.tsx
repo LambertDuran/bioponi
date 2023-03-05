1452ab7aeeadc99ac0464109cc065ae1675d8365
lambert duran <lambert.drn@gmail.com>
2023-03-05 19:29:20 +0100

    L'item de la sidebar correspondant à la page courante reste en bleu clair

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,41 +1,72 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import logo from "../assets/logo.png";
 import "./sidebar.css";
 
+const paths = [
+  "/tableau-de-bord",
+  "/parametrage",
+  "/journal",
+  "/bilan",
+  "/fiche-journalière",
+];
+const icons = [
+  "fas fa-home",
+  "fas fa-cog",
+  "fas fa-book",
+  "fas fa-balance-scale",
+  "fas fa-calendar",
+];
+const names = [
+  "Tableau de bord",
+  "Paramétrage",
+  "Journal",
+  "Bilan",
+  "Fiche Journalière",
+];
+
+interface ISidebarItem {
+  path: string;
+  icon: string;
+  name: string;
+  onClick: () => void;
+  isActive: boolean;
+}
+
+const SidebarItem = ({ path, icon, name, onClick, isActive }: ISidebarItem) => {
+  return (
+    <li className={`sidebar_${isActive ? "active" : "inactive"}`}>
+      <NavLink to={path} onClick={onClick}>
+        <i className={icon}></i>
+        {name}
+      </NavLink>
+    </li>
+  );
+};
+
 const Sidebar = () => {
+  const [activeIndex, setActiveIndex] = useState(-1);
   return (
     <div className="sidebar">
       <ul>
         <li>
           <NavLink to={"/tableau-de-bord"}>
-            <img src={logo} className="logo"></img>
+            <img alt="bioponi-logo" src={logo} className="logo"></img>
           </NavLink>
         </li>
-        <li>
-          <NavLink to={"/tableau-de-bord"}>
-            <i className="fas fa-home"></i>Tableau de bord
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to={"/parametrage"}>
-            <i className="fas fa-cog"></i>Paramétrage
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to={"/journal"}>
-            <i className="fas fa-book"></i>Journal
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to={"/bilan"}>
-            <i className="fas fa-balance-scale"></i>Bilan
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to={"/fiche-journalière"}>
-            <i className="fas fa-calendar"></i>Fiche Journalière
-          </NavLink>
-        </li>
+        {paths.map((path, index) => (
+          <SidebarItem
+            path={path}
+            icon={icons[index]}
+            name={names[index]}
+            key={index}
+            onClick={() => {
+              console.log(activeIndex);
+              setActiveIndex(index);
+            }}
+            isActive={activeIndex === index}
+          />
+        ))}
       </ul>
     </div>
   );
