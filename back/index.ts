539fc00cf01d41460df39d0d88ce6bc3e963aa39
lambert duran <lambert.drn@gmail.com>
2023-03-06 12:02:16 +0100

    Nouveau dossier /back pour le backend

---
+++ back/index.ts
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req: any, res: any) => {
+  console.log("req received!");
+  res.send("Hello World!");
+});
+
+const server = app.listen(3000, () => {
+  console.log("Server listening on port 3000!");
+});
+
+module.exports = server;
