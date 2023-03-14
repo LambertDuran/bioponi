63177e329f44826c93fd1c438d568dc1e5b16ce5
lambert duran <lambert.drn@gmail.com>
2023-03-14 14:49:57 +0100

    routes get et post, pas encore testé

---
+++ back/routes/fish.ts
@@ -0,0 +1,47 @@
+const { Request, Response } = require("express");
+const express = require("express");
+const router = express.Router();
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+router.get("/", async (req: Request, res: Response) => {
+  const fish = await prisma.fish.findMany();
+  if (!fish) return res.status(404).send("Aucun poisson trouvé!");
+  res.json(fish);
+});
+
+
+router.post("/", async (req: Request, res: Response) => {
+    // const { error } = validateFish(req.body);
+    // if (error) return res.status(400).send(error.details[0].message);
+    
+    const existingFish = await prisma.fish.findFirst({
+        where: {
+        name: req.body.name,
+        },
+    });
+    if (existingFish) return res.status(400).send("Fish already exists!");
+
+    const existingFood = await prisma.food.findFirst({
+        where: {
+        name: req.body.food.name
+        }
+    });
+    if (!existingFood) return res.status(400).send("Food doesn't exist!");
+    
+    const fish = await prisma.fish.create({
+        data: {
+        name: req.body.name,
+        weeks: req.body.weeks,
+        weights: req.body.weights,
+        food: {
+            connect: {
+            name: req.body.food.name
+            }
+        },
+    });
+    
+    if (!fish) return res.status(400).send("Prisma error creation!");
+    res.json(fish);
+}
