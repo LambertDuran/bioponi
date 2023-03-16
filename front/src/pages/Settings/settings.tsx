8173234ecc3b67c9f914978db99b7a51403f95ba
lambert duran <lambert.drn@gmail.com>
2023-03-16 14:22:52 +0100

    Ajout d'un petit fond gris sur les aliments et poissons

--- front/src/pages/Settings/settings.tsx
+++ front/src/pages/Settings/settings.tsx
@@ -12,55 +12,6 @@ import SpeciesCard from "./fishCard";
 import { getAllFish } from "../../services/fish";
 import emptyList from "../../assets/emptyList.gif";
 import "./settings.css";
-import { display } from "@mui/system";
-
-// const foods: IFood[] = [
-//   {
-//     title: "Aliment TAEC",
-//     froms: [
-//       10, 100, 150, 200, 400, 500, 1000, 1500, 1600, 1800, 2000, 2500, 3000,
-//       3500, 4000, 5000,
-//     ],
-//     tos: [
-//       100, 150, 200, 400, 500, 1000, 1500, 1600, 1800, 2000, 2500, 3000, 3500,
-//       4000, 5000, 6000,
-//     ],
-//     ranges: Array(16).fill("NEO CDC CF 20"),
-//     sizes: [
-//       5.5, 5.5, 5.5, 5.5, 7.5, 7.5, 7.5, 7.5, 7.5, 7.5, 7.5, 7.5, 7.5, 7.5, 7.5,
-//       7.5,
-//     ],
-//     foodRates: [
-//       1.55, 1.55, 1.46, 1.33, 1.23, 1.12, 0.97, 0.97, 0.97, 0.97, 0.97, 0.97,
-//       0.97, 0.97, 0.97, 0.97,
-//     ],
-//     prices: Array(16).fill(2100),
-//     distributions: Array(16).fill(100),
-//   },
-// ];
-
-// const species: IFish[] = [
-//   {
-//     name: "TAEC",
-//     weeks: [4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 82, 86],
-//     weights: [
-//       5, 20, 30, 50, 90, 110, 150, 210, 330, 400, 550, 650, 750, 2000, 2400,
-//     ],
-//     id: 1,
-//     food: iFood0,
-//     foodId: 1,
-//   },
-//   {
-//     name: "Saumon de fontaine",
-//     weeks: [4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 82, 86],
-//     weights: [
-//       7, 14, 28, 42, 56, 70, 84, 123, 162, 202, 360, 535, 672, 935, 1058,
-//     ],
-//     id: 2,
-//     food: iFood0,
-//     foodId: 1,
-//   },
-// ];
 
 export default function Settings() {
   const [openFood, setOpenFood] = useState(false);
@@ -153,7 +104,6 @@ export default function Settings() {
 
   return (
     <div className="settings_container">
-      {/* Dialogue pour l'ajout d'une nouvelle espèce d'aliment */}
       <FoodModalDialog
         title={
           isCreation
@@ -167,7 +117,6 @@ export default function Settings() {
         onFoodModification={handleFoodModification}
         isCreation={isCreation}
       />
-      {/* Dialogue pour l'ajout d'une nouvelle espèce de poisson */}
       <FishModalDialog
         title={isCreation ? "Nouvelle espèce :" : "Modification d'espèce :"}
         open={openFish}
