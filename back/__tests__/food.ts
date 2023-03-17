c1545b827c00233156686336a162515d4294d603
lambert duran <lambert.drn@gmail.com>
2023-03-17 13:30:45 +0100

    Mise en place de l'env de test

--- back/__tests__/food.ts
+++ back/__tests__/food.ts
@@ -1,37 +1,40 @@
 import * as http from "http";
-// import prisma from "./client";
+import { PrismaClient } from "@prisma/client";
 const request = require("supertest");
 
 let server: http.Server;
-
-// const iFood0 = {
-//   id: 0,
-//   name: "",
-//   froms: [10],
-//   tos: [100],
-//   ranges: ["NEO CDC CF 20"],
-//   sizes: [5.5],
-//   foodRates: [1.55],
-//   prices: [2100],
-//   distributions: [100],
-// };
-
-// // create food
-// await prisma.food.createMany({
-//   data: [iFood0],
-// });
-
-// console.log("✨ 1 food successfully created! ✨");
+const prisma = new PrismaClient();
 
 // const deleteFoods = prisma.food.deleteMany();
 // await prisma.$transaction([deleteFoods]);
 // await prisma.$disconnect();
 
-beforeEach(async () => {
+beforeAll(async () => {
   server = require("../index");
+
+  const iFood0 = {
+    name: "",
+    froms: [10],
+    tos: [100],
+    ranges: ["NEO CDC CF 20"],
+    sizes: [5.5],
+    foodRates: [1.55],
+    prices: [2100],
+    distributions: [100],
+  };
+
+  // create food
+  const res = await prisma.food.create({
+    data: iFood0,
+  });
+
+  console.log("✨ 1 food successfully created ! ✨");
 });
 
-afterEach(async () => {
+afterAll(async () => {
+  const deleteFoods = prisma.food.deleteMany();
+  await prisma.$transaction([deleteFoods]);
+  await prisma.$disconnect();
   await server.close();
 });
 
