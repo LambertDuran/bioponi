4d0366829225bad00f1b769a1f165ff4cbb7a342
lambert duran <lambert.drn@gmail.com>
2023-03-20 22:11:07 +0100

    Ré organisation de Button et de son .css

--- front/src/components/button.tsx
+++ front/src/components/button.tsx
@@ -8,6 +8,25 @@ interface IButton {
   width?: string;
 }
 
+const litteralColors = ["orange", "salmon", "yellow", "grey", "blue", "black"];
+
+const colors = [
+  "#fd7702",
+  "#ff906a",
+  "#f6bd41",
+  "#b9c6d7",
+  "#7991bd",
+  "#020d21",
+];
+const hoverColors = [
+  "#ff8e00",
+  "#ff9e7a",
+  "#f6c94a",
+  "#94a3c0",
+  "#94a3c0",
+  "#28292b",
+];
+
 export default function Button({
   title,
   children,
@@ -17,9 +36,17 @@ export default function Button({
 }: IButton) {
   return (
     <button
-      className={`bioponi_button_${color ?? "orange"}`}
+      className="bioponi_button"
       onClick={onClick}
-      style={{ "--width": width ?? "auto" } as React.CSSProperties}
+      style={
+        {
+          "--width": width ?? "auto",
+          "--color": colors[litteralColors.findIndex((c) => c === color)],
+          "--hoverColor":
+            hoverColors[litteralColors.findIndex((c) => c === color)],
+          "--textColor": color === "black" ? "white" : "black",
+        } as React.CSSProperties
+      }
     >
       <div>{children}</div>
       {title && <p style={{ paddingLeft: "0.75em" }}>{title}</p>}
