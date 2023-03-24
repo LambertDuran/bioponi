ea798e21c0b85df35953fbb6f4590a44ed54d40e
lambert duran <lambert.drn@gmail.com>
2023-03-24 15:33:43 +0100

    modification de l'interface d'action

--- front/src/interfaces/action.ts
+++ front/src/interfaces/action.ts
@@ -3,13 +3,17 @@ import IFish from "./fish";
 
 export default interface IAction {
   id: number;
+  createdAt?: Date;
+  updatedAt?: Date;
   type: string;
   date: Date;
   pool: IPool;
+  poolId?: number;
   fish: IFish;
-  totalWeight: number;
-  averageWeight: number;
-  fishNumber: number;
+  fishId?: number;
+  totalWeight?: number;
+  averageWeight?: number;
+  fishNumber?: number;
   lotName: string;
-  secondPool: IPool | null;
+  secondPool?: IPool;
 }
