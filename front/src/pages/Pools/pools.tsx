0d053c10048690ac5fefabc32a7111cfa39c12f4
lambert duran <lambert.drn@gmail.com>
2023-03-28 11:07:42 +0200

    Mise en place de la structure pour la page 'Bassins'

--- front/src/pages/Pools/pools.tsx
+++ front/src/pages/Pools/pools.tsx
@@ -1,19 +1,60 @@
 import { useEffect, useState } from "react";
 import { getAllPool } from "../../services/pool";
 import IPool from "../../interfaces/pool";
+import "./pools.css";
 
 export default function Pools() {
   const [pools, setPools] = useState<IPool[]>([]);
+  const [selectedPool, setSelectedPool] = useState<IPool | null>(null);
+
   useEffect(() => {
     async function getPools() {
       const allPool = await getAllPool();
       if (allPool && allPool.data) setPools(allPool.data);
     }
     getPools();
+    if (pools.length) setSelectedPool(pools[0]);
   }, []);
-  return (
-    <div>
-      <h1>Aucune Donnée 🙄</h1>
-    </div>
-  );
+
+  useEffect(() => {
+    if (pools.length) setSelectedPool(pools[0]);
+  }, [pools]);
+
+  useEffect(() => {
+    if (selectedPool) {
+      console.log(selectedPool);
+    }
+  }, [selectedPool]);
+
+  if (!pools.length)
+    return (
+      <div className="action_modDial_emptyList">
+        <p>
+          ⚠️ Vous devez définir un <strong>bassin</strong> et une
+          <strong> espèce de poissons</strong> dans la page
+          <strong> PARAMETRAGE</strong>⚠️
+        </p>
+      </div>
+    );
+  else
+    return (
+      <div className="pools_container">
+        <div className="pools">
+          <div>Sélectionner un bassin : </div>
+          <select
+            onChange={(e) =>
+              setSelectedPool(
+                pools.find((p) => p.number === parseInt(e.target.value))!
+              )
+            }
+          >
+            {pools.map((pool) => (
+              <option key={pool.id} value={pool.number}>
+                {pool.number}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+    );
 }
