fda9a98a9ac5897cf1ca59ef87344f93a67bcc62
lambert duran <lambert.drn@gmail.com>
2023-05-03 14:58:30 +0200

    Nouvelle fonction getComputer

--- front/src/hooks/useDatas.ts
+++ front/src/hooks/useDatas.ts
@@ -6,7 +6,49 @@ import { IComputedData } from "../interfaces/data";
 import ComputePool from "../pages/Pools/computePool";
 import { findLast } from "lodash";
 
-// async function getComputer(selectedPool: IPool | null): ComputePool {}
+async function getComputer(
+  selectedPool: IPool | null,
+  setResult?: (res: IComputedData) => void
+): Promise<ComputePool | null | undefined> {
+  if (!selectedPool) return null;
+
+  const actionWithFishId = findLast(
+    selectedPool.action!,
+    (a: IAction) => a.fishId !== null
+  );
+
+  if (!actionWithFishId) {
+    if (setResult)
+      setResult({ error: "Aucun poisson associé au bassin", data: null });
+    return null;
+  }
+
+  const fish = await getFish(actionWithFishId.fishId!);
+  if (!fish || !fish.fish) {
+    if (setResult)
+      setResult({
+        error: "Aucun poisson associé au bassin",
+        data: null,
+      });
+    return null;
+  }
+
+  if (!fish.fish.food) {
+    if (setResult)
+      setResult({
+        error: "Aucun aliment associé au poisson du bassin",
+        data: null,
+      });
+    return null;
+  }
+
+  return new ComputePool(
+    selectedPool.action!,
+    selectedPool.volume,
+    fish.fish.food,
+    fish.fish
+  );
+}
 
 function useDatas(selectedPool: IPool | null): IComputedData {
   const [result, setResult] = useState<IComputedData>({
@@ -16,44 +58,12 @@ function useDatas(selectedPool: IPool | null): IComputedData {
 
   useMemo(() => {
     async function fetchDatas() {
-      if (!selectedPool)
-        return { error: "Aucun bassin sélectionné", data: null };
-
-      const actionWithFishId = findLast(
-        selectedPool.action!,
-        (a: IAction) => a.fishId !== null
+      const computer: ComputePool | null | undefined = await getComputer(
+        selectedPool,
+        setResult
       );
 
-      if (!actionWithFishId) {
-        setResult({ error: "Aucun poisson associé au bassin", data: null });
-        return;
-      }
-
-      const fish = await getFish(actionWithFishId.fishId!);
-      if (!fish || !fish.fish) {
-        setResult({
-          error: "Aucun poisson associé au bassin",
-          data: null,
-        });
-        return;
-      }
-
-      if (!fish.fish.food) {
-        setResult({
-          error: "Aucun aliment associé au poisson du bassin",
-          data: null,
-        });
-        return;
-      }
-
-      const computer = new ComputePool(
-        selectedPool.action!,
-        selectedPool.volume,
-        fish.fish.food,
-        fish.fish
-      );
-
-      setResult(computer.computeAllData());
+      if (computer) setResult(computer.computeAllData());
     }
     fetchDatas();
   }, [selectedPool]);
